7436664af219a9b9f32dbbbfdf40a801d5ea1d09
Omar Al-Ashi <omar.ashi@hotmail.com>
2018-12-06 17:37:10 +0200

    Updates

--- precache-manifest.d0fd200ab4c7326574267b4f22c83ab2.js
+++ precache-manifest.d0fd200ab4c7326574267b4f22c83ab2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/omar-al-ashi/static/js/runtime~main.b2199c39.js"
   },
   {
-    "revision": "e40b0dc159d599ec47cb",
-    "url": "/omar-al-ashi/static/js/main.e40b0dc1.chunk.js"
+    "revision": "cc4a771729e66dc1a0ea",
+    "url": "/omar-al-ashi/static/js/main.cc4a7717.chunk.js"
   },
   {
     "revision": "0bf18d9363479f136aa1",
     "url": "/omar-al-ashi/static/js/1.0bf18d93.chunk.js"
   },
   {
-    "revision": "e40b0dc159d599ec47cb",
+    "revision": "cc4a771729e66dc1a0ea",
     "url": "/omar-al-ashi/static/css/main.52eba9e7.chunk.css"
   },
   {
-    "revision": "ee23abaf993256e6e6c47a393b0ac617",
+    "revision": "2c374361a4d22dc4d2e2aee47d6c46e4",
     "url": "/omar-al-ashi/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/omar-al-ashi/precache-manifest.0530b9202222e1bf1500cc7272c2822e.js"
+  "/omar-al-ashi/precache-manifest.d0fd200ab4c7326574267b4f22c83ab2.js"
 );
 
 workbox.clientsClaim();
