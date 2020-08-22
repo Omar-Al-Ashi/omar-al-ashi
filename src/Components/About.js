3b8fcc7b917bcd965ea51fcf4f5aed0b8af69ebd
Omar <omar.ashi@hotmail.com>
2020-08-22 18:56:49 +0300

    Minor update in About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Omar Al-Ashi Profile Pic" />
+            <img className="profile-pic" src={profilepic} alt="Omar Al-Ashi Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -29,8 +29,7 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{city}, {country}
-                   </span><br />
+
                      <span>{email}</span><br />
                       <span>{phone}</span>
 					   </p>
